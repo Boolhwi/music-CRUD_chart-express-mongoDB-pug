9777dc458e5c2b028ed3d4a1632b23c2ce8554ff
Boolhwi <dltmdcks87@naver.com>
2020-08-06 23:24:16 +0900

    1

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+let url = 'mongodb+srv://B:123321@cluster0.axyta.mongodb.net/star wars?retryWrites=true&w=majority'
+
+MongoClient.connect(url, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('music_app')
+        const pmc_collection = db.collection('pop_music_chart')
+
+        app.set('views', './views')
+        app.set('view engine', 'pug')
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(express.static('public'))
+        app.use(bodyParser.json())
+
+        app.get('/', function (req, res) {
+            db.collection('pop_music_chart').find().toArray()
+                .then(results => {
+                    res.render('index.pug', { target: results })
+                })
+                .catch(error => console.error(error))
+
+        })
+
+        app.post('/create', (req, res) => {
+            let temp = {
+                title: req.body.title,
+                artist: req.body.artist,
+                like: 0
+            }
+
+            pmc_collection.insertOne(temp)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.post('/like', (req, res) => {
+            pmc_collection.findOne(
+                {
+                    title: req.body.title,
+                    artist: req.body.artist
+                })
+                .then(results => {
+                    pmc_collection.updateOne({
+                        title: req.body.title,
+                        artist: req.body.artist
+                    }, {
+                        $set: {
+                            like: ++results.like
+                        }
+                    })
+                        .then(result => {
+                            res.redirect('/')
+                        })
+                        .catch(error => console.error(error))
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.post('/delete', (req, res) => {
+            pmc_collection.deleteOne(
+                { title: req.body.title }
+            )
+                
+                .catch(error => console.error(error))
+        })
+
+        app.listen(3000, function () {
+            console.log('listening on 3000')
+        })
+    })
+    .catch(error => console.error(error))
+
---
+++ public/main.js
@@ -0,0 +1,42 @@
+
+/*
+const create = document.querySelector('#create_btn')
+
+create.addEventListener('click', _ => {
+    fetch('/create', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            title: document.getElementById("create_title").value,
+            artist: document.getElementById("create_artist").value
+        })
+    })
+        .then(res => {
+            if (res.ok) {
+                console.log(res.json())
+                
+            }
+        })
+        
+})
+*/
+
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/delete', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            title: document.getElementById("delete-title").value
+        })
+    }).then(res => {
+        if (res.ok) return res.json()
+    })
+        .then(response => {
+            console.log(response)
+        })
+
+
+})
